803b0ec41ddf9a00990301d60194857af502abea
colincora <89736630+SFYYH@users.noreply.github.com>
2023-08-16 16:35:02 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -25,7 +25,7 @@ export default function Footer() {
       </div>
       <div className='flex space-x-4 pb-4 sm:pb-0'>
         <Link
-          href='https://github.com/ycjcl868/readme-gpt'
+          href='https://github.com/SFYYH'
           className='group'
           aria-label='TaxPal on GitHub'
         >
